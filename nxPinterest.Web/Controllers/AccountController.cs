17113be40f0bdf247d16a764dd16bb51ed00331c
mostspace <mostspace1027@gmail.com>
2021-10-09 16:30:21 +0900

    first

---
+++ nxPinterest.Web/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace nxPinterest.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Certification()
+        {
+            return View();
+        }
+    }
+}
---
+++ nxPinterest.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace nxPinterest.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
