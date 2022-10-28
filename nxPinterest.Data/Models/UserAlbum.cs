050c7b7ac531f502fe5a2947d54e7adc334f3de5
mostspace <mostspace1027@gmail.com>
2022-10-28 19:28:36 +0900

    add model UserAlbum

---
+++ nxPinterest.Data/Models/UserAlbum.cs
@@ -0,0 +1,27 @@
+﻿using nxPinterest.Data.Enums;
+using System;
+
+namespace nxPinterest.Data.Models;
+
+public class UserAlbum
+{
+    public int AlbumId { get; set; }
+
+    public string AlbumName { get; set; }
+
+    public int ContainerId { get; set; }
+
+    public string AlbumUrl { get; set; }
+
+    public AlbumType AlbumType { get; set; }
+
+    public DateTime? AlbumExpireDate { get; set; }
+
+    public bool AlbumVisibility { get; set; }
+
+    public DateTime? AlbumCreatedat { get; set; }
+
+    public DateTime? AlbumUpdatedat { get; set; }
+
+    public DateTime? AlbumDeletedat { get; set; }
+}
