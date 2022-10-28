5cf52bd4b7cde34d61545254a4b97b102daae6d2
mostspace <mostspace1027@gmail.com>
2022-10-28 19:19:11 +0900

    add enum album type

---
+++ nxPinterest.Data/Enums/AlbumType.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel;
+namespace nxPinterest.Data.Enums;
+public enum AlbumType
+{
+    [Description("共有")] 
+    AlbumShare = 0,
+    [Description("アルバム")] 
+    Album = 1
+}
