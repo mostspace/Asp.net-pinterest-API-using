95a5aef3010b3428ed0d770324db053f87e9cbe2
mostspace <mostspace1027@gmail.com>
2022-10-28 19:30:33 +0900

    add model UserAlbumMedia

---
+++ nxPinterest.Data/Models/UserAlbumMedia.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace nxPinterest.Data.Models;
+
+public class UserAlbumMedia
+{
+    public int AlbumMediaId { get; set; }
+
+    public int UserMediaId { get; set; }
+
+    public string UserMediaName { get; set; }
+
+    public int ContainerId { get; set; }
+
+    public DateTime? AlbumMediaCreatedat { get; set; }
+
+    public DateTime? AlbumMediaUpdatedat { get; set; }
+
+    public DateTime? AlbumMediaDeletedat { get; set; }
+}
