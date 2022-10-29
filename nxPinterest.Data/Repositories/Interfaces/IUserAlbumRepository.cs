241e7d0fa704900156cfd6c39e58dac90312c248
mostspace <mostspace1027@gmail.com>
2022-10-29 19:02:13 +0700

    add repo UserAlbum and UserAlbumMedia

---
+++ nxPinterest.Data/Repositories/Interfaces/IUserAlbumRepository.cs
@@ -0,0 +1,9 @@
+﻿using nxPinterest.Data.Models;
+
+namespace nxPinterest.Data.Repositories.Interfaces
+{
+    public interface IUserAlbumRepository : IBaseRepository<UserAlbum>
+    {
+        // you can implementing another method as you like
+    }
+}
---
+++ nxPinterest.Data/Repositories/UserAlbumRepository.cs
@@ -0,0 +1,12 @@
+﻿using nxPinterest.Data.Models;
+using nxPinterest.Data.Repositories.Interfaces;
+
+namespace nxPinterest.Data.Repositories
+{
+    public class UserAlbumRepository : BaseRepository<UserAlbum>, IUserAlbumRepository
+    {
+        public UserAlbumRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
