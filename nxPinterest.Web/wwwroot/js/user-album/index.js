68b8a88e68905817e60bb5cff021e89fc560adaf
mostspace <mostspace1027@gmail.com>
2022-10-30 23:16:59 +0700

    add js handle save album

---
+++ nxPinterest.Web/wwwroot/js/user-album/index.js
@@ -0,0 +1,48 @@
+﻿var userAlbumController = function () {
+
+    this.initialize = function () {
+        registerEvents();
+    }
+
+    function registerEvents() {
+
+        $('body').on('click', '#createUserMediaFolderButton', function (e) {
+            e.preventDefault();
+            var $links = window.selectedMediaSrcList;
+            var $mediaid = window.selectedMediaIdList;
+            if (!window.selectedMediaIdList || $mediaid.length === 0 ||
+                !window.selectedMediaSrcList || $links.length === 0) {
+                alert("please choose the image !");
+                return;
+            }
+            var $albumName = $('#createUserMediaFolderName').val();
+            if ($albumName === '' || $albumName == null) {
+                alert("Album Name is required !");
+            }
+            var $associateAlbum = $mediaid.map(function (v, k, a) {
+                return { UserMediaId: v, MediaUrl: $links[k] };
+            });
+
+            $.ajax({
+                url: "/UserAlbum/Create",
+                type: 'post',
+                dataType: 'json',
+                data: {
+                    AlbumName: $albumName,
+                    UserAlbumMedias: $associateAlbum
+                },
+                cache: false,
+                success: function (data) {
+                    alert("success");
+                },
+                error: function () {
+                    alert("error");
+                }
+            });
+
+
+        });
+    };
+
+
+}
