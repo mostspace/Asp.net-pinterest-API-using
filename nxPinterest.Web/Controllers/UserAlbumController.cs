db750c2a31f04bddfa069786dec4186d3232c320
mostspace <mostspace1027@gmail.com>
2022-10-30 18:22:32 +0700

    add controller user album

---
+++ nxPinterest.Web/Controllers/UserAlbumController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace nxPinterest.Web.Controllers
+{
+    [Authorize]
+    public class UserAlbumController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
