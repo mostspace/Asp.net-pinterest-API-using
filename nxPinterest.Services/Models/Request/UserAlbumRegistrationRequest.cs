a363ffe4626559f849fb4701a44457e95aceb3cc
mostspace <mostspace1027@gmail.com>
2022-10-30 20:37:07 +0700

    add create model UserAlbum request

---
+++ nxPinterest.Services/Models/Request/UserAlbumRegistrationRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace nxPinterest.Services.Models.Request
+{
+    public class CreateUserAlbumRequest
+    {
+        public string AlbumName { get; set; }
+
+        public List<UserAlbumMediaRequest> UserAlbumMedias { get; set; }
+    }
+
+    public class UserAlbumMediaRequest
+    {
+        public int UserMediaId { get; set; }
+
+        public string MediaUrl { get; set; }
+    }
+}
