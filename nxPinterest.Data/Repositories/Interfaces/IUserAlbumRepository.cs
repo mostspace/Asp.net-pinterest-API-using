f5820ac94cc4c16c309096fe83ca8c967b03ef33
mostspace <mostspace1027@gmail.com>
2022-11-01 21:13:15 +0900

    add get album by user

--- nxPinterest.Data/Repositories/Interfaces/IUserAlbumRepository.cs
+++ nxPinterest.Data/Repositories/Interfaces/IUserAlbumRepository.cs
@@ -1,4 +1,5 @@
-﻿using nxPinterest.Data.Models;
+﻿using System.Collections.Generic;
+using nxPinterest.Data.Models;
 using System.Threading.Tasks;
 
 namespace nxPinterest.Data.Repositories.Interfaces
@@ -8,5 +9,7 @@ namespace nxPinterest.Data.Repositories.Interfaces
         (int albumId, string albumName) IsUserAlbumAlreadyExists(string albumName);
 
         Task<bool> CheckExpiryDayAlbum(int albumId);
+
+        Task<IEnumerable<UserAlbum>> GetAlbumByUser(string userId);
     }
 }
--- nxPinterest.Services/Interfaces/IUserAlbumService.cs
+++ nxPinterest.Services/Interfaces/IUserAlbumService.cs
@@ -1,4 +1,6 @@
-﻿using nxPinterest.Services.Models.Request;
+﻿using nxPinterest.Data.Models;
+using nxPinterest.Services.Models.Request;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace nxPinterest.Services.Interfaces
@@ -6,5 +8,7 @@ namespace nxPinterest.Services.Interfaces
     public interface IUserAlbumService
     {
         Task<bool> Create(CreateUserAlbumRequest model, string userId);
+
+        Task<IEnumerable<UserAlbum>> GetAlbumByUser(string userId);
     }
 }
