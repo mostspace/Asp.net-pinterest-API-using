fcb69dcc41fcfffc790f5474ec5a5d0056552607
mostspace <mostspace1027@gmail.com>
2022-11-04 15:20:18 +0900

    add create user album shared request

---
+++ nxPinterest.Services/Models/Request/CreateUserAlbumSharedRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace nxPinterest.Services.Models.Request;
+
+public class CreateUserAlbumSharedRequest
+{
+    public class CreateUserAlbumRequest
+    {
+        public string AlbumName { get; set; } = "noname";
+
+        public DateTime? AlbumExpireDate { get; set; }
+
+        public List<UserAlbumMediaRequest> UserAlbumMedias { get; set; }
+    }
+
+    public class UserAlbumMediaRequest
+    {
+        public int UserMediaId { get; set; }
+
+        public string MediaUrl { get; set; }
+    }
+}
