88a593befa5f7ffba045caf42ddfbfba6b8690ca
mostspace <mostspace1027@gmail.com>
2022-11-08 07:17:17 +0700

    add method get id by url

--- nxPinterest.Data/Repositories/Interfaces/IUserAlbumRepository.cs
+++ nxPinterest.Data/Repositories/Interfaces/IUserAlbumRepository.cs
@@ -12,5 +12,7 @@ namespace nxPinterest.Data.Repositories.Interfaces
         Task<bool> CheckExpiryDayAlbum(int albumId);
 
         Task<IEnumerable<UserAlbumViewModel>> GetAlbumByUser(string userId);
+
+        Task<int> GetAlbumIdByUrl(string url);
     }
 }
