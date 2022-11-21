42b7c7a6d80abcdda35187448ec57890eee65cac
mostspace <mostspace1027@gmail.com>
2022-11-21 19:43:44 +0900

    update home controler and update view model

--- nxPinterest.Web/Controllers/HomeController.cs
+++ nxPinterest.Web/Controllers/HomeController.cs
@@ -78,7 +78,11 @@ namespace nxPinterest.Web.Controllers
 
             //よく使用されているアルバムの一覧 TODO
            var album = await userAlbumService.GetAlbumUserByContainer(user[0].Id);
-           vm.AlbumList = album.Select(a => a.AlbumName).ToList();
+           vm.AlbumList = album.Select(n=> new nxPinterest.Web.Models.UserAlbumViewModel
+           {
+               AlbumName = n.AlbumName,
+               AlbumUrl = n.AlbumUrl
+           }).ToList();
 
             //登録画面で使用されているタグ候補
             vm.ImageRegistrationVM.SuggestTagsList = vm.TagList;
