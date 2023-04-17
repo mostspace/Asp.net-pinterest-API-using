f68bd6eba15cbf52a851f4a9400e345e36d7b109
mostspace <mostspace1027@gmail.com>
2023-04-18 06:59:47 +0900

    一括編集画面の移動とVMの作成

---
+++ nxPinterest.Web/Models/EditMultiSelectImageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace nxPinterest.Web.Models
+{
+    public class EditMultiSelectImageViewModel : BaseViewModel
+    {
+        public int DetailsMediaId { get; set; }
+        public IList<Data.Models.UserMedia> UserMediaList { get; set; } = new List<Data.Models.UserMedia>();
+    }
+
+}
